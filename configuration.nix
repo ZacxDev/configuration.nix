67cd00feb52c490cabc6ef088b0b69ac072540cc
Zachary Lowden <hello@zacx.dev>
2025-06-15 12:02:15 -0500

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -20,7 +20,8 @@
   networking.nameservers = ["1.1.1.1" "8.8.8.8"];
 
   # Set your time zone.
-  time.timeZone = "Europe/Madrid";
+  #time.timeZone = "America/Buenos_Aires";
+  time.timeZone = "America/Winnipeg";
 
   # The global useDHCP flag is deprecated, therefore explicitly set to false here.
   # Per-interface useDHCP will be mandatory in the future, so this generated config
@@ -40,9 +41,10 @@
 
   # Enable the X11 windowing system.
 
-  #environment.pathsToLink = [ "/libexec" ]; # links /libexec from derivations to /run/current-system/sw 
+  #environment.pathsToLink = [ "/libexec" ]; # links /libexec from derivations to /run/current-system/sw
 
-  #services.dbus.packages = with pkgs; [ gnome3.dconf ];
+  #services.dbus.packages = with pkgs; [ dconf ];
+  services.displayManager.defaultSession = "none+i3";
 
   services.xserver = {
     enable = true;
@@ -55,10 +57,9 @@
     };
 
     displayManager = {
-      defaultSession = "none+i3";
       lightdm = {
         enable = true;
-        background = "#000000";
+        background = "/home/zach/wallpaper.png";
         greeters = {
           mini = {
             user = "zach";
@@ -117,31 +118,31 @@
   };
 
   # Use PCH for audio (analog should be device 0 in `aplay -l`)
-  environment.etc."asound.conf".text = ''
-    defaults.pcm.!card "PCH"
-    defaults.ctl.!card "PCH"
-  '';
+  #environment.etc."asound.conf".text = ''
+    #defaults.pcm.!card "PCH"
+    #defaults.ctl.!card "PCH"
+  #'';
 
   # Enable the GNOME Desktop Environment.
-  #services.xserver.displayManager.gdm.enable = true;
-  #services.xserver.desktopManager.gnome.enable = true;
-  
+  #services.displayManager.gdm.enable = true;
+  #services.desktopManager.gnome.enable = true;
+
   # Configure keymap in X11
-  # services.xserver.layout = "us";
-  # services.xserver.xkbOptions = "eurosign:e";
+  # services.layout = "us";
+  # services.xkbOptions = "eurosign:e";
 
   # Enable CUPS to print documents.
   # services.printing.enable = true;
 
   # Enable sound.
-  sound.enable = true;
-  # hardware.pulseaudio.enable = true;
+  #sound.enable = true;
+  #hardware.pulseaudio.enable = true;
 
   # Enable touchpad support (enabled default in most desktopManager).
-  services.xserver.libinput.enable = true;
+  services.libinput.enable = true;
 
   # Enable opengl
-  hardware.opengl.enable = true;
+  hardware.graphics.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.zach = {
@@ -159,7 +160,18 @@
     };
   };
 
-  nixpkgs.config.allowUnfree = true;
+  nixpkgs.config = {
+    allowUnfree = true;
+  };
+
+nixpkgs.config.packageOverrides = pkgs: {
+  pidgin-with-plugins = pkgs.pidgin.override {
+    plugins = with pkgs; [
+      purple-discord
+      purple-slack
+    ];
+  };
+};
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
@@ -167,28 +179,53 @@
     acpi
     iw
     lm_sensors
-  ];
-
-  environment.defaultPackages = with pkgs; [
-    i3-resurrect
-
-    alacritty
-
-    discord
-    tdesktop
-    brave
-    nyxt
-
-    kube3d
+    wireguard-tools
 
     git
     htop
+
+    i3-resurrect
+
+    dunst
+    libnotify
+
+    pavucontrol
+
+    cifs-utils
+  ];
+
+  environment.defaultPackages = with pkgs; [
+
+    teamviewer
+
+    brave
+    firefox
+    opera
+    qutebrowser
+
+    flameshot
+
+    gimp
+
+    sublime-merge
+    dbeaver-bin
+    mongodb-compass
+
+    prismlauncher
+
+    xfce.thunar
   ];
 
   environment.extraInit = ''
-  export I3CONFIG_DMENU_INCLUDE="firefox brave discord alacritty slack nyxt telegram-desktop shutter"
+  alias braveChatGpt="brave https://chatgpt.com"
+  export I3CONFIG_DMENU_INCLUDE="firefox brave opera alacritty flameshot mongodb-compass dbeaver ferdium teamviewer qutebrowser thunar braveChatGpt whatsie"
   export I3CONFIG_DEFAULT_TERMINAL="alacritty"
   '';
+
+  environment.sessionVariables = {
+    GTK_USE_PORTAL = "1";
+    XDG_CURRENT_DESKTOP = "i3";
+  };
 
   programs.light.enable = true;
 
@@ -204,12 +241,69 @@
 
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
+  #services.teamviewer.enable = true;
+
+  services.gnome.tracker.enable = true;
+  services.gnome.tracker-miners.enable = true;
+
+  hardware.bluetooth.enable = true; # enables support for Bluetooth
+  hardware.bluetooth.powerOnBoot = true;
+  services.blueman.enable = true;
+
+  services.printing.enable = true;
+
+  services.avahi = {
+    enable = true;
+    nssmdns4 = true;
+    openFirewall = true;
+  };
 
   # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
+   #networking.firewall.allowedTCPPorts = [ 43179 7844 ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
+
+  networking.firewall = {
+    allowedUDPPorts = [ 51820 7844 80 443 58012 8180 ]; # Clients and peers can use the same port, see listenport
+    allowedTCPPorts = [ 7844 80 443 58012 8180 ];
+  };
+  # Enable WireGuard
+  networking.wg-quick.interfaces = {
+    # "wg0" is the network interface name. You can name the interface arbitrarily.
+    wg0 = {
+      # Determines the IP address and subnet of the client's end of the tunnel interface.
+      address = [ "10.0.0.3/32" ];
+      #listenPort = 51820; # to match firewall allowedUDPPorts (without this wg uses random port numbers)
+
+      # Path to the private key file.
+      #
+      # Note: The private key can also be included inline via the privateKey option,
+      # but this makes the private key world-readable; thus, using privateKeyFile is
+      # recommended.
+      privateKeyFile = "/home/zach/.config/wireguard-keys/private";
+
+      peers = [
+        # For a client configuration, one peer entry for the server will suffice.
+
+        {
+          # Public key of the server (not a file path).
+          publicKey = "SVWl4qGDCBTakNW8IFJ5WQI7ojiryPSLz7BB+o4O50M=";
+
+          # Forward all the traffic via VPN.
+          allowedIPs = [ "192.168.50.0/24" ];
+          #allowedIPs = [ "0.0.0.0/0" ];
+          # Or forward only particular subnets
+          #allowedIPs = [ "10.100.0.1" "91.108.12.0/22" ];
+
+          # Set this to the server IP and port.
+          endpoint = "24.79.61.66:51820"; # ToDo: route to endpoint not automatically configured https://wiki.archlinux.org/index.php/WireGuard#Loop_routing https://discourse.nixos.org/t/solved-minimal-firewall-setup-for-wireguard-client/7577
+
+          # Send keepalives every 25 seconds. Important to keep NAT tables alive.
+          persistentKeepalive = 25;
+        }
+      ];
+    };
+  };
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
@@ -217,7 +311,9 @@
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "21.05"; # Did you read the comment?
+  system.stateVersion = "23.05"; # Did you read the comment?
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
   boot.kernel.sysctl = {
     "fs.inotify.max_user_watches" = 1048576;
@@ -239,6 +335,8 @@
     lidSwitch = "lock";
     extraConfig = "IdleAction=lock";
   };
+
+  services.tumbler.enable = true; # Thumbnail support for images
 
   # Prevent nix-direnv cached shells from being gabage-collected
   nix.extraOptions = ''
@@ -278,5 +376,16 @@
     lockerCommand = "${pkgs.i3lock}/bin/i3lock -c #0a1b31 -u";
   };
 
-}
+  nix.settings.substituters = [
+    "https://nix-community.cachix.org"
+  ];
 
+  nix.settings.trusted-public-keys = [
+    "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+  ];
+
+  swapDevices = [
+    { device = "/dev/disk/by-uuid/dd0c4e40-afa1-4e53-9754-39e50d41c0c5"; }
+  ];
+
+}
